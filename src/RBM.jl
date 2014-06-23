d23f4fd8f4b71cd6cc176c54973ec145b6603455
Keyan Ghazi-Zahedi <keyan.zahedi@googlemail.com>
2014-06-23 11:04:26 +0200

    Initialisation and weight scaling function
    - Create function (now without bins)
    - Standard create function
    - Random weight initialisation function
    - Output units bias initialisation based on data distribution
    - Weight scaling function
    - RBM copy function
    - test cases

--- src/RBM.jl
+++ src/RBM.jl
@@ -1,20 +1,23 @@
 module RBM
 
-type RBM_t # checked
-  bins::Int64        # number of bins for discretized data
-  n::Int64           # number of output nodes
-  m::Int64           # number of hidden nodes
-  k::Int64           # number of input nodes
-  uditer::Int64      # number of up-down passes when sampling
-  alpha::Float64     # learning rate
-  momentum::Float64  # contribution of previous gradient
-  numepochs::Int64   # number of training epochs
-  batchsize::Int64   # size of training data batches
-  mem::Int64         # time window of inputs (standard ist 1)
-  W::Matrix{Float64} # interaction weigths between hidden and outputs
-  V::Matrix{Float64} # interaction weights between hidden and inputs
-  b::Vector{Float64} # bias weights for outputs
-  c::Vector{Float64} # bias weights for hiddens
+export RNN_t, rbm_copy
+export rbm_create, rbm_create_with_standard_values
+export rbm_init_random_weights!, rbm_init_visible_bias!
+export rbm_rescale_weights!
+
+type RBM_t 
+  n::Int64            # number of output nodes
+  m::Int64            # number of hidden nodes
+  k::Int64            # number of input nodes
+  uditer::Int64       # number of up-down passes when sampling
+  alpha::Float64      # learning rate
+  momentum::Float64   # contribution of previous gradient
+  numepochs::Int64    # number of training epochs
+  batchsize::Int64    # size of training data batches
+  W::Matrix{Float64}  # interaction weights between hidden and outputs
+  V::Matrix{Float64}  # interaction weights between hidden and inputs
+  b::Vector{Float64}  # bias weights for outputs
+  c::Vector{Float64}  # bias weights for hiddens
   vW::Matrix{Float64} # for the momentum
   vV::Matrix{Float64} # for the momentum
   vb::Vector{Float64} # for the momentum
@@ -22,27 +25,79 @@ type RBM_t # checked
 end
 
 
-function rbm_create(bins, n, m, k, uditer, alpha, momentum, numepochs, batchsize, mem, randomise)
-  if randomise
-    W = (2.0 .* rand((m,n)) .- 1.0) .* 0.1 # random numbers in [-0.1, 0.1]
-    V = (2.0 .* rand((m,k)) .- 1.0) .* 0.1 # random numbers in [-0.1, 0.1]
-    b = (2.0 .* rand(n)     .- 1.0) .* 0.1 # random numbers in [-0.1, 0.1]
-    c = (2.0 .* rand(m)     .- 1.0) .* 0.1 # random numbers in [-0.1, 0.1]
-  else
-    W = zeros((m, n))
-    V = zeros((m, k))
-    b = zeros(n)
-    c = zeros(m)
-  end
+function rbm_create(n::Int64, m::Int64, k::Int64, uditer::Int64,
+  alpha::Float64, momentum::Float64, numepochs::Int64, batchsize::Int64)
+
+  W = zeros((m, n))
+  V = zeros((m, k))
+  b = zeros(n)
+  c = zeros(m)
 
   vW = zeros((m, n))
   vV = zeros((m, k))
   vb = zeros(n)
   vc = zeros(m)
 
-  return RBM_t(bins, n, m, k, uditer, alpha, momentum, numepochs, batchsize,
-               mem, W, V, b, c, vW, vV, vb, vc)
+  return RBM_t(n, m, k, uditer, alpha,
+               momentum, numepochs, batchsize,
+               W, V, b, c, vW, vV, vb, vc)
 end
 
+function rbm_create_with_standard_values(n::Int64, m::Int64, k::Int64)
+  rbm_create(n, m, k, 10, 0.1, 0.5, 10000, 50)
+end
+
+function rbm_copy(src::RBM_t)
+  copy = rbm_create(src.n, src.m, src.k, src.uditer, src.alpha, src.momentum, src.numepochs, src.batchsize)
+  copy.W  = deepcopy(src.W)
+  copy.V  = deepcopy(src.V)
+  copy.b  = deepcopy(src.b)
+  copy.c  = deepcopy(src.c)
+  copy.vW = deepcopy(src.vW)
+  copy.vV = deepcopy(src.vV)
+  copy.vb = deepcopy(src.vb)
+  copy.vc = deepcopy(src.vc)
+  return copy
+end
+
+function rbm_init_random_weights!(rbm::RBM_t)
+  rbm.W = rand((rbm.m,rbm.n)) .* 0.01
+  rbm.V = rand((rbm.m,rbm.k)) .* 0.01
+end
+
+function rbm_init_visible_bias!(rbm::RBM_t, data::Array{Int64,2})
+  # each row of the data must contain be of type {0,1}^k
+  @assert (size(data)[2] == rbm.n) "rbm_init_visible_bias!: Each row of the data array must be of type {0,1}^k"
+
+  l = size(data)[1]
+  for i=1:rbm.n
+    s = sum(data[:,i])
+    if s == l
+      p =  (l-1) / l
+      rbm.b[i] = log2(p/(1.0 - p))
+    elseif s == 0
+      rbm.b[i] = 0.0
+    else
+      p = s / l
+      rbm.b[i] = log2(p/(1.0 - p))
+    end
+  end
+end
+
+function rbm_rescale_weights!(rbm::RBM_t, abs_maximum::Float64)
+  @assert (abs_maximum > 0.0) "rbm_rescale_weights!: abs_maximum must be > 0.0"
+  mw = maximum(abs(rbm.W))
+  mv = maximum(abs(rbm.V))
+  if mw > mv
+    m = mw
+  else
+    m = mv
+  end
+  if m > abs_maximum
+    factor = abs_maximum / m
+    rbm.W = rbm.W .* factor
+    rbm.V = rbm.V .* factor
+  end
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,109 @@ using RBM
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+
+# test initialisation
+rbm = rbm_create(11, 12, 13, 14, 0.1, 0.2, 100, 15)
+@test rbm.n         == 11
+@test rbm.m         == 12
+@test rbm.k         == 13
+@test rbm.uditer    == 14
+@test rbm.alpha     == 0.1
+@test rbm.momentum  == 0.2
+@test rbm.numepochs == 100
+@test rbm.batchsize == 15
+
+# test standard initialisation
+rbm = rbm_create_with_standard_values(1, 2, 3)
+@test rbm.n         == 1
+@test rbm.m         == 2
+@test rbm.k         == 3
+@test rbm.uditer    == 10
+@test rbm.alpha     == 0.1
+@test rbm.momentum  == 0.5
+@test rbm.numepochs == 10000
+@test rbm.batchsize == 50
+
+# test visible bias initialisation
+r = [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] [1, 0, 0, 0, 0, 0, 0, 0, 0, 0] [1, 1, 0, 0, 0, 0, 0, 0, 0, 0] [1, 1, 1, 0, 0, 0, 0, 0, 0, 0] [1, 1, 1, 1, 0, 0, 0, 0, 0, 0] [1, 1, 1, 1, 1, 0, 0, 0, 0, 0] [1, 1, 1, 1, 1, 1, 0, 0, 0, 0] [1, 1, 1, 1, 1, 1, 1, 0, 0, 0] [1, 1, 1, 1, 1, 1, 1, 1, 0, 0] [1, 1, 1, 1, 1, 1, 1, 1, 1, 0] [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+rbm = rbm_create_with_standard_values(11, 1, 1)
+rbm_init_visible_bias!(rbm, r)
+
+for i=1:11
+  if i == 1
+    s = 0.0
+  elseif i == 11
+    p = (10 - 1.0) / 10.0
+    s = log2( p / ( 1 - p))
+  else
+    p = (i - 1.0) / 10.0
+    s = log2( p / ( 1 - p))
+  end
+  @test rbm.b[i] == s
+end
+
+# test rbm copying
+src   = rbm_create_with_standard_values(10, 10, 10)
+src.W = rand(10,10)
+src.V = rand(10,10)
+src.b = rand(10)
+src.c = rand(10)
+dest  = rbm_copy(src)
+
+@test dest.n  == src.n
+@test dest.m  == src.m
+@test dest.k  == src.k
+@test dest.W  == src.W
+@test dest.V  == src.V
+@test dest.b  == src.b
+@test dest.c  == src.c
+@test dest.vW == src.vW
+@test dest.vV == src.vV
+@test dest.vb == src.vb
+@test dest.vc == src.vc
+
+dest.W[1,1] = dest.W[1,1] * 2
+@test dest.W != src.W
+
+# test weight scaling
+rbm = rbm_create_with_standard_values(10, 10, 10)
+rbm.W = rand(10,10) * 100
+rbm.V = rand(10,10) * 100
+cpy = rbm_copy(rbm)
+rbm_rescale_weights!(rbm, 10.0)
+mw = maximum(abs(rbm.W))
+mv = maximum(abs(rbm.V))
+if mw > mv
+  m = mw
+else
+  m = mv
+end
+@test m == 10.0
+
+mw = maximum(abs(cpy.W))
+mv = maximum(abs(cpy.V))
+if mw > mv
+  mx = mw
+else
+  mx = mv
+end
+
+factor = m / mx
+
+for i=1:size(rbm.W)[1]
+  for j=1:size(rbm.W)[2]
+    rbm.W[i,j]  = cpy.W[i,j] * factor
+    rbm.W[i,j] != cpy.W[i,j]
+  end
+end
+
+for i=1:size(rbm.V)[1]
+  for j=1:size(rbm.V)[2]
+    rbm.V[i,j]  = cpy.V[i,j] * factor
+    rbm.V[i,j] != cpy.V[i,j]
+  end
+end
+
+# test of assertion
+#= rbm = rbm_create(11, 12, 13, 14, 0.1, 0.2, 100, 15) =#
